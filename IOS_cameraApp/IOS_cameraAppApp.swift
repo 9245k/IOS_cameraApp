a7c191d3c928ac5fe98c3e70d2d9c061a39822df
9245k <9245.kobayashi@gmail.com>
2025-07-20 11:59:20 +0900

    second commit

---
+++ IOS_cameraApp/IOS_cameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_cameraAppApp.swift
+//  IOS_cameraApp
+//
+//  Created by koba on 2025/07/20.
+//
+
+import SwiftUI
+
+@main
+struct IOS_cameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
