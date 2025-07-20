913e88ce3d19a84f51bac58a4d11e5ed8a195b3f
9245k <9245.kobayashi@gmail.com>
2025-07-20 11:56:32 +0900

    first code

---
+++ IOS_cameraAppTests/IOS_cameraAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_cameraAppTests.swift
+//  IOS_cameraAppTests
+//
+//  Created by koba on 2025/07/20.
+//
+
+import Testing
+@testable import IOS_cameraApp
+
+struct IOS_cameraAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
